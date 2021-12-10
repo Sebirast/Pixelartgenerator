c6ebe31b37e6a35fb68ad87c16469a8344dd63e6
Sebastian Rast <sebastian.rast@outlook.com>
2021-12-10 09:30:47 +0100

    added canvas and corresponding css

--- Code/index.php
+++ Code/index.php
@@ -4,21 +4,25 @@
 <html>
     <head>
         <title>Pixelartgenerator</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         
         <link rel="stylesheet" href="Style/style.css">
     </head>
 
     <body>
-        <div class="title"></div>
-        <div class="wrapper">
-            <div class="card">
-                <!-- <canvas class="canvas" id="imageDisplay"></canvas> -->
-                <h1>first card</h1>
-            </div>
+        <div class="container">
+            <div class="title"></div>
+            <div class="row">
+                <div class="col-8">
+                    <h1>1. Column</h1>
+                    <canvas id="Canvas"></canvas>
+                </div>
 
-            <div class="card">
-                <h1>second card</h1>
-            </div>
+                <div class="col">
+                    <h1>2. Column</h1>
+                </div>
+            </div>    
         </div>
     </body>
 </html>
