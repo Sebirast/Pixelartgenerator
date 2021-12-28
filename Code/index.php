b6b0d521f5d0455f8329766ec2337e63b559b641
Sebastian Rast <sebastian.rast@outlook.com>
2021-12-28 15:21:52 +0100

    added pixelartprocessing.js

--- Code/index.php
+++ Code/index.php
@@ -73,6 +73,7 @@
 
         <script src="Scripts/script.js" type="module"></script>
         <script src="Scripts/utilities.js" type="module"></script>
+        <script src="Scripts/pixelartProcessing.js" type="module"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
         <!-- imports for modals (it didn't work without them and I don't know why xD) -->
