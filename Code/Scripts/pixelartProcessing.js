89222022dbb4a37a90b2eb9fa78dfa0251b8a733
Sebastian Rast <sebastian.rast@outlook.com>
2022-01-03 16:05:58 +0100

    added pixel class and chunk class (not tested)

--- Code/Scripts/pixelartProcessing.js
+++ Code/Scripts/pixelartProcessing.js
@@ -1,7 +1,62 @@
 import { aSlider, pixelSizeSlider } from "./script.js";
 
+let processingTypes = {
+    median, 
+    average, 
+    middlePixel, 
+    range
+};
 
-export function changeAValue() {
-    // let data = localStorage.getItem("imageData");
-    // console.log(data);
+class Pixel {
+    constructor(r, g, b, a) {
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+        this.alpha = a;
+    }
+
+    getData() {
+        return {
+            red: this.red,
+            green: this.green, 
+            blue: this.blue,
+            alpha: this.alpha
+        };
+    }
+    
+    toPixelArray(data) {
+        let array = []; 
+        for(let i = 0; i < data.length; i += 4) {
+            let p = new Pixel(
+                data[i],
+                data[i + 1],
+                data[i + 2],
+                data[i + 3]
+            )
+            array.push(p);
+        }
+        return array;
+    }
+}
+
+class Chunk {
+    constructor(content, length){
+        this.content = content;
+        this.sideLength = length;
+
+        if(this.sideLength % 2 == 1) {
+            this.middlePixel = Math.ceil(Math.pow(this.sideLength, 2)/2) - 1;
+        }
+        else {
+            // TODO think of a solution for even sidelenghts
+        }
+    }   
+    
+    getMiddlePixel() {
+        return new Pixel(this.content[this.middlePixel],
+            this.content[this.middlePixel + 1],
+            this.content[this.middlePixel + 2],
+            this.content[this.middlePixel + 3]
+        );
+    }
 }
